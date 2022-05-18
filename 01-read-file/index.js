bb6aab8322e2931e491a820069b7ca05dd6d8c47
thelastandrew <thelastandrew@gmail.com>
2022-05-18 05:30:17 +0300

    feat: add read file script

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, 'text.txt'), (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
